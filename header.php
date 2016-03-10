dc77772376a0551cc139daa0a7dede2d3c02dea3
Samuel Villavicencio <savillavicencio@csumb.edu>
2016-03-09 16:56:45 -0800

    initial commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<div class=header>
+    <img src="img/hs.png" height=180px>
+
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Events</a>
+        <a href="#">Projects</a>
+        <a href="#">Members</a>
+        <a href="#">Contact</a>
+    </nav>
+
+    <soc>
+        <a href="#"><img src="img/fb.png"></a>
+        <a href="#"><img src="img/tw.png"></a>
+    </soc>
+</div>
+
+<?php
+    $current_page=basename($_SERVER['PHP_SELF']);   // use to set banner
+?>
